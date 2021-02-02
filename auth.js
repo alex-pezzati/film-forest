ca3f56ac37aaca95eb8e4cd0199ad9d73fe239a3
Hussein Eid <husseineid@mochamacbook.myfiosgateway.com>
2021-02-02 11:44:17 -0500

    login phase 3

--- auth.js
+++ auth.js
@@ -10,6 +10,13 @@ const logoutUser = (req, res) => {
     delete req.session.auth
 }
 
+const requireAuth = (req, res, next) => {
+    if (!res.locals.authenticated) {
+        return res.redirect('/user/login')
+    }
+    return next()
+}
+
 module.exports = {
     loginUser,
     logoutUser
