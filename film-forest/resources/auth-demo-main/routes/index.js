cb08138d529848c85c26e963e9f515dc69e1d6eb
MelodyLayne <melodylayne79@gmail.com>
2021-02-02 16:38:53 -0600

    cleanup file systems

--- film-forest/resources/auth-demo-main/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Super cool app' });
-});
-
-module.exports = router;
