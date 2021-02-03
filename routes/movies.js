cabc7dac59118cf4e8307e2acc9186c16d666994
laura button <llalla02@msn.com>
2021-02-03 15:47:53 -0700

    Movie page phase 1

---
+++ routes/movies.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const { Movie, Review, Vote,  } = require('../db/models')
+
+const { asyncHandler, csrfProtection } = require('./utils.js');
+const router = express.Router()
+
+// router.get('/', asyncHandler(async (req, res) => {
+//     const movies = await db.Movie.findAll({ order: ['title'] })
+//     res.render('movie-list', { title: 'Movies', movies })
+// }))
+
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const movieId = req.params.id
+    const movie = await Movie.findByPk( movieId)
+    res.render('movie', { title: movie.title, movie })
+}))
+
+router.post('/:id(\\d+)', asyncHandler(async (req, res)=> {
+    const movieId = req.params.id
+    const review = req.body
+    await Review.create({
+    userId: 1,
+    review: review.review,
+    movieId
+    })
+    res.redirect(`/movies/${movieId}`)
+}))
+
+
+
+module.exports = router
