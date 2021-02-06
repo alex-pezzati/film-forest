77d62daca1e6f9eac70afe773227d0460c13268e
alex-pezzati <aapezzati@gmail.com>
2021-02-06 15:36:35 -0500

    minor edits

--- routes/my-movies.js
+++ routes/my-movies.js
@@ -4,7 +4,9 @@ const { asyncHandler, csrfProtection } = require('./utils.js');
 const router = express.Router()
 
 router.get('/', asyncHandler(async(req, res) => {
-    const movies = await Movie.findAll()
+    const movies = await Movie.findAll({
+        order: [[ 'title' ]]
+    })
 
     res.render('my-movies', { movies })
 }))
