998c5c01e6c84627152c244fd4393e6882dbf54f
alex-pezzati <aapezzati@gmail.com>
2021-02-07 14:51:24 -0500

    finally got the my movies queries to work

--- routes/my-movies.js
+++ routes/my-movies.js
@@ -3,28 +3,60 @@ const { Dashboard, Movie, MoviesDashboard, Review, Vote, User } = require('../db
 const { asyncHandler, csrfProtection } = require('./utils.js');
 const router = express.Router()
 
+// router.get('/', asyncHandler(async (req, res) => {
+//     const movies = await Movie.findAll({
+//         order: [[ 'title' ]]
+//     });
+
+//     res.render('my-movies', { movies });
+// }));
+
 router.get('/', asyncHandler(async (req, res) => {
-    const movies = await Movie.findAll({
-        order: [[ 'title' ]]
+    const userId = res.locals.user.id;
+
+    // by default grab 'My Movies' user dashboard
+    const myDashboards = await Dashboard.findAll({
+        where: {
+            userId,
+            name: 'My Movies'
+        }
     });
 
-    res.render('my-movies', { movies });
+    const dashboardId = myDashboards[0].id;
+
+    const myDashboardMovies = await MoviesDashboard.findAll({
+        where: {
+            dashboardId
+        }
+    })
+
+    const movies = await Movie.findAll();
+
+
+    console.log(myDashboards, 'MY DASHBOARD OBJECT?!!!?!?!?!?')
+    console.log(myDashboardMovies, 'MY DASHBORD OVIES OBJECT?!!!?!?!?!?')
+    // console.log(movies, 'ANOTHER OBJECT????')
+    // console.log(myDashboards[0].Movie, 'MOVIE OBJECT?!!!?!?!?!?')
+
+
+    res.render('my-movies', { myDashboardMovies, movies });
 }));
+
+
 
 
 router.post('/:id(\\d+)', asyncHandler(async (req, res) => {
     const movieId = req.params.id;
     // const dashboardId = req.body;
-    // const userId = res.locals.user.id;
 
-    console.log('IM INSIDE THIS POST!!!!!!!!!!!!!!!!!!')
-    console.log(movieId)
-    console.log(req.body.dashboardId)
+    // console.log('IM INSIDE THIS POST!!!!!!!!!!!!!!!!!!')
+    // console.log(movieId)
+    // console.log(req.body.dashboardId)
 
     try {
         await MoviesDashboard.create({
-        dashboardId: req.body.dashboardId,
-        movieId: req.params.id
+            dashboardId: req.body.dashboardId,
+            movieId: req.params.id
         });
     } catch (e) {
         console.error(e)
