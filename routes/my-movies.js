15c9b8715984688794810f7525d4efc4087b1e1c
alex-pezzati <aapezzati@gmail.com>
2021-02-07 18:51:15 -0500

    progress on my-movies page

--- routes/my-movies.js
+++ routes/my-movies.js
@@ -3,21 +3,11 @@ const { Dashboard, Movie, MoviesDashboard, Review, Vote, User } = require('../db
 const { asyncHandler, csrfProtection } = require('./utils.js');
 const router = express.Router()
 
-// router.get('/', asyncHandler(async (req, res) => {
-//     const movies = await Movie.findAll({
-//         order: [[ 'title' ]]
-//     });
-
-//     res.render('my-movies', { movies });
-// }));
-
 router.get('/', asyncHandler(async (req, res) => {
-    const userId = res.locals.user.id;
-
     // by default grab 'My Movies' user dashboard
     const myDashboards = await Dashboard.findAll({
         where: {
-            userId,
+            userId: res.locals.user.id,
             name: 'My Movies'
         }
     });
@@ -32,16 +22,51 @@ router.get('/', asyncHandler(async (req, res) => {
 
     const movies = await Movie.findAll();
 
+    res.render('my-movies', { myDashboardMovies, movies });
+}));
 
-    console.log(myDashboards, 'MY DASHBOARD OBJECT?!!!?!?!?!?')
-    console.log(myDashboardMovies, 'MY DASHBORD OVIES OBJECT?!!!?!?!?!?')
-    // console.log(movies, 'ANOTHER OBJECT????')
-    // console.log(myDashboards[0].Movie, 'MOVIE OBJECT?!!!?!?!?!?')
 
+router.get('/to-watch', asyncHandler(async (req, res) => {
+    const myDashboards = await Dashboard.findAll({
+        where: {
+            userId: res.locals.user.id,
+            name: 'To Watch'
+        }
+    });
+
+    const dashboardId = myDashboards[0].id;
+
+    const myDashboardMovies = await MoviesDashboard.findAll({
+        where: {
+            dashboardId
+        }
+    })
+
+    const movies = await Movie.findAll();
 
     res.render('my-movies', { myDashboardMovies, movies });
 }));
 
+router.get('/watched', asyncHandler(async (req, res) => {
+    const myDashboards = await Dashboard.findAll({
+        where: {
+            userId: res.locals.user.id,
+            name: 'Have Watched'
+        }
+    });
+
+    const dashboardId = myDashboards[0].id;
+
+    const myDashboardMovies = await MoviesDashboard.findAll({
+        where: {
+            dashboardId
+        }
+    })
+
+    const movies = await Movie.findAll();
+
+    res.render('my-movies', { myDashboardMovies, movies });
+}));
 
 
 
@@ -52,17 +77,18 @@ router.post('/:id(\\d+)', asyncHandler(async (req, res) => {
     // console.log('IM INSIDE THIS POST!!!!!!!!!!!!!!!!!!')
     // console.log(movieId)
     // console.log(req.body.dashboardId)
-
-    try {
         await MoviesDashboard.create({
             dashboardId: req.body.dashboardId,
             movieId: req.params.id
         });
-    } catch (e) {
-        console.error(e)
-    }
+
 
     res.redirect(`/movies/${movieId}`);
+}));
+
+router.delete('/:id(\\d+)', asyncHandler(async (req, res) => {
+
+
 }));
 
 
